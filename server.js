d1cb3b2f222d95f3f5b98202c550f1f03ea0c63a
joshua machuta <meathead-boy@hotmail.com>
2024-07-08 21:58:19 -0400

    uncomment db connection

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app = configure(app);
 var server = app.listen(app.get('port'), function () {
   console.log('Web server up: http://localhost:' + app.get('port'));
 
-  // mongodb.connect('mongodb://localhost:27017/moviedb', function () {
-  //   console.log('Connected to DB');
-  // });
+  mongodb.connect('mongodb://localhost:27017/moviedb', function () {
+    console.log('Connected to DB');
+  });
 });
